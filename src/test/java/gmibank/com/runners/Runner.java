edaf68e9b582dd42c4da4b24af7239708f8fe023
Mustafa Gecer <h.musti.27@gmail.com>
2022-04-04 18:44:18 +0200

    yeni

---
+++ src/test/java/gmibank/com/runners/Runner.java
@@ -0,0 +1,21 @@
+package gmibank.com.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        strict = true,
+        plugin = {"html:target/default-cucumber-reports",
+                "json:target/json-reports/cucumber.json",
+                "junit:target/xml-report/cucumber.xml"},
+        features = "src/test/resources/features",
+        glue = "gmibank/stepdefinitions",
+        tags = "@Api_Testing_ders",
+        dryRun = true
+
+)
+public class Runner {
+
+}
